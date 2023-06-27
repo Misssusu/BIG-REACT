829b9eb96ff083c12189ec6cb021819c7686b368
Misssusu <suyuan326@qq.com>
2023-06-27 17:45:38 +0800

    feat: 实现打包流程

---
+++ scripts/rollup/react.config.js
@@ -0,0 +1,50 @@
+import { resolvePkgPath, getPackageJSON, getBaseRollupPlugins } from './util';
+import generatePackageJson from 'rollup-plugin-generate-package-json';
+
+const { name, module } = getPackageJSON('react');
+// react包的路径
+const pkgPath = resolvePkgPath(name);
+// react产物路径
+const pkgDistPath = resolvePkgPath(name, true);
+
+export default [
+	{
+		input: `${pkgPath}/${module}`,
+		output: {
+			file: `${pkgDistPath}/index.js`,
+			name: 'react',
+			format: 'umd' // umd兼容commonjs和es module
+		},
+		plugins: [
+			...getBaseRollupPlugins(),
+			generatePackageJson({
+				inputFolder: pkgPath,
+				outputFolder: pkgDistPath,
+				baseContents: ({ name, description, version }) => ({
+					name,
+					description,
+					version,
+					main: 'index.js'
+				})
+			})
+		]
+	},
+	// rimraf dist 删除dist文件
+	// jsx runtime
+	{
+		input: `${pkgPath}/src/jsx.ts`,
+		output: [
+			{
+				file: `${pkgDistPath}/jsx-dev-runtime.js`,
+				name: 'jsx-dev-runtime.js',
+				format: 'umd' // umd兼容commonjs和es module
+			},
+			{
+				file: `${pkgDistPath}/jsx-runtime.js`,
+				name: 'jsx-runtime.js',
+				format: 'umd' // umd兼容commonjs和es module
+			}
+		],
+		plugins: getBaseRollupPlugins()
+	}
+];
