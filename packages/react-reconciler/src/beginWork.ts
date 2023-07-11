9ed9d67a76208329c1912ed1afc2698af6ca0700
Misssusu <suyuan326@qq.com>
2023-07-11 18:43:03 +0800

    feat: 实现Reconciler架构

---
+++ packages/react-reconciler/src/beginWork.ts
@@ -0,0 +1,8 @@
+import { FiberNode } from './fiber';
+
+// 递归中的递阶段
+export const beginWork = (fiber: FiberNode) => {
+	// 比较，返回子fiberNode
+	const childFiberNode: FiberNode = fiber;
+	return childFiberNode;
+};
---
+++ packages/react-reconciler/src/workLoop.ts
@@ -0,0 +1,59 @@
+import { FiberNode } from './fiber';
+import { beginWork } from './beginWork';
+import { completeWork } from './completeWork';
+
+let workInProgress: FiberNode | null = null;
+
+function prepareFreshStack(fiber: FiberNode) {
+	workInProgress = fiber;
+}
+
+function renderRoot(root: FiberNode) {
+	// 初始化
+	prepareFreshStack(root);
+
+	do {
+		try {
+			workLoop();
+			break;
+		} catch (e) {
+			console.warn('workLoop发生错误', e);
+			workInProgress = null;
+		}
+	} while (true);
+}
+
+function workLoop() {
+	while (workInProgress !== null) {
+		performUnitOfWork(workInProgress);
+	}
+}
+
+function performUnitOfWork(fiber: FiberNode) {
+	// 向下查找对比
+	const next = beginWork(fiber);
+	fiber.memoizedProps = fiber.pendingProps;
+
+	if (next === null) {
+		completeUnitOfWork(fiber);
+	} else {
+		workInProgress = next;
+	}
+}
+
+function completeUnitOfWork(fiber: FiberNode) {
+	// 向上开始查找对比
+	let node: FiberNode | null = fiber;
+
+	do {
+		completeWork(node);
+		const sibling = node.sibling;
+
+		if (sibling !== null) {
+			workInProgress = sibling;
+			return;
+		}
+		node = node.return;
+		workInProgress = node;
+	} while (node !== null);
+}
